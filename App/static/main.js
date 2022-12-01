53487325de53cf753b8054863fe9fdf2ebfbfea0
ShivsTKD <shivanmaharaj.sm@gmail.com>
2022-12-01 21:32:30 +0000

    Fixed dynamic form fields in add_author.html

--- App/static/main.js
+++ App/static/main.js
@@ -1,33 +1,63 @@
 const form = document.getElementById("authors_form");
 let i = 1;
 
+function test(){
+    let node = document.createElement('div');
+    let html = "<div>This is real <h1>BIG</h1> a test</div>";
+    let node2 = document.createTextNode("<div>This is a test</div>");
+    node.innerHTML = html;
+    form.appendChild(node);
+    form.appendChild(node2);
+};
+
 function add_author(){
     i += 1;
+    let row = document.createElement('div');
+    row.classList.add('row');
+    row.classList.add('collection-item');
+    row.classList.add('valign-wrapper');
     let html = `
-        <div class="row collection-item valign-wrapper">
-            <div style="height: 100%;" class="col s1">${i}</div>
-            <div class="col s3 input-field">
-                <label for="fname">First Name</label>
-                <input class="input-field col s12" type="text" name="fname">
-            </div>
+        <div style="height: 100%;" class="col s1">${i}</div>
+        <div class="col s3 input-field">
+            <label for="fname">First Name</label>
+            <input class="input-field col s12" type="text" name="fname">
+        </div>
 
-            <div class="col s4 input-field">
-                <label for="lname">Last Name</label>
-                <input class="input-field col s12" type="text" name="lname">
-            </div>
+        <div class="col s4 input-field">
+            <label for="lname">Last Name</label>
+            <input class="input-field col s12" type="text" name="lname">
+        </div>
 
-            <div class="col s4 input-field">
-                <label for="email">Email</label>
-                <input class="input-field col s12" type="text" name="email">
-            </div>
+        <div class="col s4 input-field">
+            <label for="email">Email</label>
+            <input class="input-field col s12" type="text" name="email">
         </div>
     `;
-    form.appendChild(html);
+    row.innerHTML = html;
+    form.appendChild(row);
+    // form.innerHTML += `
+    //     <div class="row collection-item valign-wrapper">
+    //         <div style="height: 100%;" class="col s1">${i}</div>
+    //         <div class="col s3 input-field">
+    //             <label for="fname">First Name</label>
+    //             <input class="input-field col s12" type="text" name="fname">
+    //         </div>
+
+    //         <div class="col s4 input-field">
+    //             <label for="lname">Last Name</label>
+    //             <input class="input-field col s12" type="text" name="lname">
+    //         </div>
+
+    //         <div class="col s4 input-field">
+    //             <label for="email">Email</label>
+    //             <input class="input-field col s12" type="text" name="email">
+    //         </div>
+    //     </div>
+    // `;
 };
 
 function remove_author(){
     i -= 1;
-    // let node = form.lastElementChild;
     form.removeChild(form.lastElementChild);
 };
 
