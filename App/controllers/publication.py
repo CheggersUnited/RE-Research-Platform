212bd352b252e6fb3ed58020cdad94ef57ca394e
ShivsTKD <shivanmaharaj.sm@gmail.com>
2022-12-02 18:37:16 +0000

    Just need to implement search

--- App/controllers/publication.py
+++ App/controllers/publication.py
@@ -37,5 +37,5 @@ def get_publication(id):
 def get_all_publications():
     return Publication.query.all()
 
-def get_publication_by_field(field):
-    return Publication.query.filter_by(field=field).all
+def get_publications_by_field(field):
+    return Publication.query.filter_by(field=field).all()
--- App/views/index.py
+++ App/views/index.py
@@ -1,11 +1,27 @@
 from flask import Blueprint, redirect, render_template, request, send_from_directory,url_for
 from flask_login import login_required
+from App.controllers import get_publications_by_field
 
 index_views = Blueprint('index_views', __name__, template_folder='../templates')
 
-@index_views.route('/', methods=['GET'])
+@index_views.route('/', methods=['GET', 'POST'])
 @login_required
 def index_page():
+    field = request.args.get('field')
+    search_type = request.args.get('search_type')
+    data = request.form
+    if data:
+        print(data['search'])
+        
+
+    if search_type == None:
+        search_type = True
+    
+    publications = []
+    if field:
+        print(field)
+        publications = get_publications_by_field(field)
+        print(publications)
     fields = [  "Climate Change", "Cancer Research", "Music Therapy", "Ocean Acidification", 
                 "Urban Development", "Mental Health", "Sustainable Agriculture"]
-    return render_template('index.html', fields=fields)
+    return render_template('index.html', fields=fields, publications=publications, search_type=search_type)
